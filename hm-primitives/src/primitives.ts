1d99a348a78fa847b59a2d63232d24639eb1bdb3
Julien Blanchet <j55blanchet@gmail.com>
2020-02-19 17:35:38 -0500

    Working now

--- hm-primitives/src/primitives.ts
+++ hm-primitives/src/primitives.ts
@@ -1,10 +1,25 @@
 
 
+var rootSeg: Segment
 
 function setup() {
   createCanvas(400, 400);
+
+  rootSeg = new Segment(null, 100, PI / 2);
+  
+  rootSeg.addChild(100, PI / 3, 70)
+         .addChild(70, PI / 2, 50)
+         .addChild(50, PI * 0.7, 30)
 }
 
 function draw() {
   background(220);
+
+  translate(width / 2, height / 2);
+  rootSeg.draw();
+
+  rootSeg.recurse((s: Segment) => {
+    s.angle += 0.1;
+    s.angle %= 2 * PI;
+  });
 }
--- hm-primitives/src/segment.ts
+++ hm-primitives/src/segment.ts
@@ -1,18 +1,46 @@
 
 class Segment {
-  parent: Segment;
-  children: Segment[];
-  length: number;
+
+  children: {pos: number, seg: Segment}[] = [];
   
-  constructor(parent: Segment, length: number){
-    this.parent = parent;
-    this.length = length;
-    this.children = [];
+  constructor(public parent: Segment | null, 
+              public length: number, 
+              public angle: number){
+  }
+
+  addChild(pos: number, ang: number, len: number): Segment{
+    let seg = new Segment(this, len, ang)
+    this.children.push({
+      pos: pos,
+      seg: seg
+    })
+    
+    return seg
   }
   
   draw() {
-   for(let c of this.children){
-      c.draw(); 
-   }  
+    push()
+
+    rotate(this.angle)
+
+    strokeWeight(sqrt(this.length))
+    line(0, 0, this.length, 0)
+    
+    
+    for(let c of this.children){
+      push()
+      translate(c.pos, 0, 0)
+      c.seg.draw()
+      pop()
+    }   
+    
+    pop()
+  }
+
+  recurse(f: (s: Segment) => void){
+    f(this);
+    for(let c of this.children){
+      c.seg.recurse(f)
+    }
   }
 }

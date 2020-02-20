90243382f2b0501bc397de4918eb38e31462df79
Julien Blanchet <j55blanchet@gmail.com>
2020-02-19 23:01:17 -0500

    Good creative progress

--- hm-primitives/src/primitives.ts
+++ hm-primitives/src/primitives.ts
@@ -1,65 +1,200 @@
 import * as p5 from 'p5'
 
-var rootSeg: Segment
-
 var isLooping = true
 var transformer: P5Transformer;
 
+var canvas: p5.Renderer
 var buffer: p5.Graphics
 
 let colors: p5.Color[]
 
+var rootSegs: {loc: p5.Vector, seg: Segment}[]
+
+var angleRates: number[]
+
+
+var params = {
+  rootSegs: {
+    min: 1,
+    max: 1
+  },
+  depth: {
+    min: 4,
+    max: 5
+  },
+  length: {
+    min: 0.0625,
+    max: 0.2125
+  },
+  angle: {
+    startMin: 0,
+    startMax: TWO_PI,
+    incMin: 0.01,
+    incMax: 0.15,
+    randomCount: 10,
+    randomRates: true
+  },
+  drawLines: false,
+  drawDots: true,
+  color: {
+    offset: 0,
+  }
+}
+
+
 function setup() {
-  createCanvas(800, 800);
   
-  frameRate(30)
-
-  buffer = createGraphics(width, height)
-  transformer = new P5Transformer()
-
-  https://paletton.com/#uid=75-0S0kmqsTcVHEidwXrnpUtBkl
   colors = [
-    color("#C71D2D"),
-    color("#CE8B1E"),
-    color("#1D4C87"),
-    color("#33AC19")
+      // https://paletton.com/#uid=75-0S0kmqsTcVHEidwXrnpUtBkl
+    color(199, 29,  45),
+    color(206, 139, 30),
+    color(72,  111,  175),
+    color(51,  172, 25),
+    // https://paletton.com/#uid=7420S0kmqsTcVHEidwXrnpUtBkl
+    color(97,  92,  180),
+    color(144, 46,  150),
+    color(174, 200, 29),
+    color(206, 165, 30)
   ]
 
-  rootSeg = new Segment(null, 140, PI / 2);
+  params = {
+    rootSegs: {
+      min: 1,
+      max: 1
+    },
+    depth: {
+      min: 4,
+      max: 5
+    },
+    length: {
+      min: 0.0625,
+      max: 0.2125
+    },
+    angle: {
+      startMin: 0,
+      startMax: TWO_PI,
+      incMin: 0.01,
+      incMax: 0.15,
+      randomCount: 10,
+      randomRates: false
+    },
+    drawLines: false,
+    drawDots: true,
+    color: {
+      offset: floor(random(colors.length))
+    }
+  }
+
+
+
+  if (params.angle.randomRates) {
+    angleRates = []
+    for(let i = 0; i < params.angle.randomCount; i++) {
+      angleRates.push(random(params.angle.incMin, params.angle.incMax))
+    }
+  } else {
+    angleRates = [
+      0.10,
+      0.05,
+      0.025,
+      0.075
+    ]
+  }
+
+  rootSegs = []
+
+  canvas = createCanvas(windowWidth, windowHeight - 40);
   
-  rootSeg.addChild(140, PI / 3, 100)
-         .addChild(100, PI / 2, 60)
-         .addChild(60, PI, 100)
+  frameRate(60)
+
+  buffer = createGraphics(width, height)
+  buffer.background(60);
+  transformer = new P5Transformer()
+
+  let minDim = min(width, height)
+  const rootSegCount = round(random(params.rootSegs.min, params.rootSegs.max))
+  for(let i = 0; i < rootSegCount; i++) {
+
+    let length = random(params.length.min * minDim, params.length.max * minDim)
+    let startAng = random(params.angle.startMin, params.angle.startMax)
+
+    let rootSeg = new Segment(null, length, startAng);
+    let seg = rootSeg
+    let targetDepth = round(random(params.depth.min, params.depth.max))
+    
+    for(let seg = rootSeg, depth = 1; depth < targetDepth; depth ++) {
+      seg = seg.addChild(seg.length, random(TWO_PI), random(params.length.min * minDim, params.length.max * minDim))
+    }
+
+    
+    rootSegs.push({
+      // random(width), random(height)
+      loc: createVector(0, 0),
+      seg: rootSeg
+    })
+  }
+
+  createButton("Save without arms").mouseClicked(saveBuffer)
+  createButton("Save with arms").mouseClicked(saveAll)
+}
+
+function saveBuffer(){
+  save(buffer, "multidof-art.png")
+}
+function saveAll(){
+  save(canvas, "multidof-arms-art.png")
 }
 
 function printLocation(seg: Segment) {
-  console.log(`seg[${seg.id}] at point: (${transformer.x}, ${transformer.y})`)
-  buffer.strokeWeight(5)
-  buffer.stroke(colors[seg.id])
-  buffer.point(transformer.x, transformer.y);
+  // console.log(`seg[${seg.id}] at point: (${transformer.x}, ${transformer.y})`)
+  
+  let ci = (params.color.offset + seg.id) % colors.length;
+  let c = colors[ci]
+  buffer.stroke(c)
+  // console.log(`Seg #${seg.id}\tcolor: ${ci}`)
+
+  transformer.push()
+  transformer.translate(seg.length, 0);
+
+  let loc = {x: transformer.x, y: transformer.y}
+
+  if (params.drawLines && seg.prevLoc) 
+  {
+    buffer.strokeWeight(1.5)
+    buffer.line(seg.prevLoc.x, seg.prevLoc.y, loc.x, loc.y)
+  }
+  if (params.drawDots)
+  {
+    buffer.strokeWeight(5)
+    buffer.point(transformer.x, transformer.y);
+  }
+  
+
+  seg.prevLoc = loc
+
+  transformer.pop();
 }
 
 function draw() {
-  background(240);
   
   transformer.reset()
 
   image(buffer, 0, 0)
   transformer.translate(width / 2, height / 2);
 
-  rootSeg.draw(transformer, printLocation);
+  for(let rootSeg of rootSegs) {
+    // transformer.push()
+    // transformer.translate(rootSeg.loc.x, rootSeg.loc.y)
+    rootSeg.seg.draw(transformer, printLocation);
+    // transformer.pop()
+  }
 
-  let angleRates = [
-    0.1, 
-    0.05,
-    0.15, 
-    0.07
-  ]
-
-  rootSeg.recurse((s: Segment) => {
-    s.angle += angleRates[s.id];
-    s.angle %= 2 * PI;
-  });
+  for(let rootSeg of rootSegs) {
+    rootSeg.seg.recurse((s: Segment) => {
+      s.angle += angleRates[s.id % angleRates.length];
+      s.angle %= 2 * PI;
+    });
+  }
 }
 
 function mouseClicked() {
--- hm-primitives/src/segment.ts
+++ hm-primitives/src/segment.ts
@@ -1,20 +1,21 @@
-
 let gid = 0
 
 class Segment {
 
   id: number
   children: {pos: number, seg: Segment}[] = [];
+  prevLoc: {x: number, y: number} | null = null
   
   constructor(public parent: Segment | null, 
               public length: number, 
-              public angle: number){
+              public angle: number,
+              public depth: number = 1){
     this.id = gid
     gid += 1
   }
 
   addChild(pos: number, ang: number, len: number): Segment{
-    let seg = new Segment(this, len, ang)
+    let seg = new Segment(this, len, ang, this.depth + 1)
     this.children.push({
       pos: pos,
       seg: seg
@@ -31,15 +32,19 @@ class Segment {
     let sr_len = sqrt(this.length)
     let h_sr_len = sr_len / 2
 
-    stroke(30)
-    fill(117, 53, 6)
-    strokeWeight(2)
+    stroke(30, 50)
+    fill(30, 100)
+    strokeWeight(h_sr_len)
     rectMode(CENTER)
     rect(this.length / 2, 0, this.length + sr_len, sr_len, h_sr_len)
 
-    strokeWeight(1)
-    fill(225, 53, 6)
-    ellipse(0, 0, h_sr_len, h_sr_len)
+    // strokeWeight(1)
+    // fill(225, 53, 6, 100)
+    // ellipse(0, 0, h_sr_len, h_sr_len)
+
+    noStroke()
+    fill(255, 150)
+    ellipse(this.length, 0, h_sr_len, h_sr_len);
     
     if (callback) {
       callback(this)
